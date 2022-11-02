16ce6c214d17cc95986e577d0d9f0f6cd0accb87
Nacu George <nacu_george@hotmail.com>
2022-11-02 17:16:08 +0200

    solution

--- face_dettection/src/components/Logo/Logo.js
+++ face_dettection/src/components/Logo/Logo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Tilt from 'react-tilt'
 import './Logo.css'
 import logo from './Logo.png'
  
@@ -8,11 +7,10 @@ import logo from './Logo.png'
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
         <div className="Tilt-inner pa3">
             <img style={{paddingTop: '5px'}} src={logo} alt='The Logo'/>
             </div>
-        </Tilt>
+        
         </div>
     );
 }
