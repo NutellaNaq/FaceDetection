f008681e673e8b9e666e06d5b3b669777acddcb9
Nacu George <nacu_george@hotmail.com>
2022-11-03 09:59:25 +0200

    Updates

---
+++ precache-manifest.07db346f6323ef65e4de44618126cac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8add96b1046204feab55",
+    "url": "/FaceDetection/static/css/main.8aef79e5.chunk.css"
+  },
+  {
+    "revision": "8add96b1046204feab55",
+    "url": "/FaceDetection/static/js/main.6f514dda.chunk.js"
+  },
+  {
+    "revision": "94f5cc47cbd667388d58",
+    "url": "/FaceDetection/static/js/runtime~main.c79bf926.js"
+  },
+  {
+    "revision": "36fc436f77a4ba6a463b",
+    "url": "/FaceDetection/static/css/2.5a73cfc0.chunk.css"
+  },
+  {
+    "revision": "36fc436f77a4ba6a463b",
+    "url": "/FaceDetection/static/js/2.0f254ca8.chunk.js"
+  },
+  {
+    "revision": "6515e591187948cb8a6a",
+    "url": "/FaceDetection/static/js/3.f79af52e.chunk.js"
+  },
+  {
+    "revision": "679ec104b56a91a4bb0f418b7e1a38a5",
+    "url": "/FaceDetection/index.html"
+  }
+];
